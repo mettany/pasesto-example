ef0f63e99d58b61398d65f49e04e1413c94b2853
Yoan Mettan <y.mettan@gmail.com>
2020-12-11 23:26:33 +0100

    ft - read paseto token

--- src/main/java/dev/mettan/paseto/Paseto.java
+++ src/main/java/dev/mettan/paseto/Paseto.java
@@ -1,22 +1,21 @@
 package dev.mettan.paseto;
 
 import com.google.gson.GsonBuilder;
+import dev.paseto.jpaseto.PasetoException;
 import dev.paseto.jpaseto.Pasetos;
 import dev.paseto.jpaseto.io.gson.GsonSerializer;
 import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.edec.EdECObjectIdentifiers;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.pqc.jcajce.provider.BouncyCastlePQCProvider;
 
 import java.io.IOException;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.Security;
+import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.time.Instant;
 import java.util.Map;
 
@@ -24,6 +23,26 @@ public class Paseto {
 
     private final String privateKey = "4f67efbf5bac0ac01c30f359e603c79d45b2b81d503c1c8b0fada469a3b9d50e2d9f331a7ebb8a870dc642a2351bad47ff82b88f096aff3e85ef543fed9cf7b8";
     private final String publicKey = "2d9f331a7ebb8a870dc642a2351bad47ff82b88f096aff3e85ef543fed9cf7b8";
+
+    /**
+     * This function is a basic sample of how to verify and read a PASETO
+     * token with JPaseto.
+     * @param token PASETO token
+     * @return true if the token is valid else false
+     */
+    public boolean verifyToken(String token) {
+        try {
+            PublicKey publicKey = getPublicKey();
+            Pasetos.parserBuilder()
+                    .setPublicKey(publicKey)
+                    .build()
+                    .parse(token);
+            return true;
+        } catch (PasetoException e) {
+            return false;
+        }
+
+    }
 
     /**
      * This method is returning the generated PASETO token
@@ -66,6 +85,25 @@ public class Paseto {
             PrivateKeyInfo privateKeyInfo = new PrivateKeyInfo(new AlgorithmIdentifier(EdECObjectIdentifiers.id_Ed25519), new DEROctetString(pvKey));
             PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(privateKeyInfo.getEncoded());
             return keyFact.generatePrivate(pkcs8KeySpec);
+        } catch (NoSuchAlgorithmException | IOException | InvalidKeySpecException e) {
+            throw new Error(e);
+        }
+    }
+
+    /**
+     * Generate a java.security.PublicKey from a hex encoded private
+     * key. You should store it in a secure location. For the example
+     * the keys are directly hardcoded in the code
+     * @return java.security.PublicKey
+     */
+    private PublicKey getPublicKey() {
+        Security.addProvider(new BouncyCastleProvider());
+        byte[] pvKey = FormatUtils.decodeHex(publicKey);
+        try {
+            KeyFactory keyFact = KeyFactory.getInstance("Ed25519");
+            SubjectPublicKeyInfo publicKeyInfo = new SubjectPublicKeyInfo(new AlgorithmIdentifier(EdECObjectIdentifiers.id_Ed25519), pvKey);
+            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(publicKeyInfo.getEncoded());
+            return keyFact.generatePublic(x509EncodedKeySpec);
         } catch (NoSuchAlgorithmException | IOException | InvalidKeySpecException e) {
             throw new Error(e);
         }
--- src/test/java/dev/mettan/paseto/PasetoTest.java
+++ src/test/java/dev/mettan/paseto/PasetoTest.java
@@ -4,12 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Base64;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PasetoTest {
 
-    private Paseto paseto = new Paseto();
+    private final Paseto paseto = new Paseto();
 
     @Test
     public void signTokenShouldReturnV2PublicToken() {
@@ -21,6 +20,24 @@ public class PasetoTest {
         assertEquals("v2", splited[0]);
         assertEquals("public", splited[1]);
         assertEquals("optionalFooterAsString", new String(Base64.getDecoder().decode(splited[3])));
+    }
+
+    @Test
+    public void verifyTokenWithInvalidTokenShouldReturnFalse() {
+        String token = "v2.public.eyJpZCI6ICI0MTBkZjI5Ni04OWQ1LTQzODAtODQyMy02ZjJkNzMwNDA3NDQiLCAibmFtZSI6ICJSYW5kYWxsIERlZ2dlcyIsICJleHAiOiAiMjAxOS0xMC0xMFQxMTowMzoyNC0wNzowMCJ9xe6hZBYn8IZoJmgL9k1VjTcl7Dz4T-lo2FvIxeFXQNtNY3QAyCaa5XW-29n-9nV-beU6z7P-YF97lPFvnPfnDA.eyJraWQiOiAiMTIzNDUifQ";
+
+        boolean result = paseto.verifyToken(token);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void verifyTokenWithValidTokenShouldReturnTrue() {
+        String token = "v2.public.eyJzdWIiOiJleGFtcGxlLXN1YmplY3QiLCJvdGhlckN1c3RvbUNsYWltIjoib3RoZXJWYWx1ZSIsImlzcyI6ImV4YW1wbGUtaXNzdWVyIiwiaWF0IjoiMjAyMC0xMi0xMVQyMzoyMDozMSswMTAwIiwieW91ckN1c3RvbUNsYWltIjoidmFsdWUifWz2qqLLRS02_nRvWFKAMNIEcbWFnDLBgf8DyGIMopyiTHihSZAAMNPdPiUrvw-gYWdvRwJAUTfFnygKpg2l-wc.b3B0aW9uYWxGb290ZXJBc1N0cmluZw";
+
+        boolean result = paseto.verifyToken(token);
+
+        assertTrue(result);
     }
 
 }
